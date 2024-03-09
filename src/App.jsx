b8c5d72a1ad2ca760a2d0312f8ab1947e4daa9ce
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2024-03-09 05:07:47 -0300

    creacion de grilla sin responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import imagen1 from './assets/imagen1.png'
+import imagen2 from "./assets/imagen2.png"
+import imagen3 from "./assets/imagen3.png"
+import imagen4 from "./assets/imagen4.png"
+import imagen5 from "./assets/imagen5.png"
+import imagen6 from "./assets/imagen6.png"
+import imagen7 from "./assets/imagen7.png"
+import imagen8 from "./assets/imagen8.png"
+import imagen9 from "./assets/imagen9.png"
+import imagen10 from "./assets/imagen10.png"
+import imagen11 from "./assets/imagen11.png"
+import imagen12 from "./assets/imagen12.png"
+import imagen13 from "./assets/imagen13.png"
+import imagen14 from "./assets/imagen14.png"
+import imagen15 from "./assets/imagen15.png"
+import imagen16 from "./assets/imagen16.png"
+import imagen17 from "./assets/imagen17.png"
+import imagen18 from "./assets/imagen18.png"
+import imagen19 from "./assets/imagen19.png"
+import imagen20 from "./assets/imagen20.png"
+
+import { useState } from 'react';
+function Card({ imagen, contactMe }) {
+  return (
+    <div className="card">
+      <img src={imagen} alt="Imagen seleccionada" />
+      <h3>Título de la imagen</h3>
+      <p>Descripción de la imagen</p>
+      <button onClick={contactMe}>Contáctame</button>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showCard, setShowCard] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleMouseEnter = (imagen) => {
+    setSelectedImage(imagen);
+    setShowCard(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowCard(false);
+  };
+
+  const imagenes = [
+    imagen1,
+    imagen2,
+    imagen3,
+    imagen4,
+    imagen5,
+    imagen6,
+    imagen7,
+    imagen8,
+    imagen9,
+    imagen10,
+    imagen11,
+    imagen12,
+    imagen13,
+    imagen14,
+    imagen15,
+    imagen16,
+    imagen17,
+    imagen18,
+    imagen19,
+    imagen20
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="galeria">
+      {imagenes.map((imagen, index) => (
+        <div
+          className={`imagen ${selectedImage === imagen ? 'selected' : ''}`}
+          key={index}
+          onMouseEnter={() => handleMouseEnter(imagen)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img src={imagen} alt="Imagen" />
+          {showCard && selectedImage === imagen && <Card imagen={selectedImage} /* onClose={handleMouseLeave} */ />}
+        </div>
+      ))}
+    </div>
+  </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import App from "../App";
+import "./Layout.css";
+/* import Header from "./header/Header";
+import { Footer } from "./footer/Footer"; */
+
+export default function Layout(props) {
+  return (
+    <div className="container">
+        <div className="contenedor">
+        <App />
+        </div>
+    </div>
+  );
+}
