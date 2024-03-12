eb0c047d2ce5337f17425450ccb000dc720366ff
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2024-03-12 15:51:47 -0300

    cambios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client'
 import Layout from './Components/Layout.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <Layout/>
-  </React.StrictMode>,
 )
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </BrowserRouter>
+); */
