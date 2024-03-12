b8c53c51724ff717d4885a4731295f5027bd5d2f
Nicolas Lopez <nicolasgonzalolopez@hotmail.com>
2024-03-12 16:05:57 -0300

    cambios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Layout from './Components/Layout.jsx'
+/* import Layout from './Components/Layout.jsx' */
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Layout/>
+    <App/>
 )
 /* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
